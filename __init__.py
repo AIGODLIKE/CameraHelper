82ca12c49f63366d2c0a4b6b9790bfbb1e348163
1029920178@qq.com <1029910278@qq.com>
2023-12-08 18:32:35 +0800

    更新版本

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 bl_info = {
     "name": "Camera Helper",
     "author": "AIGODLIKE社区,Atticus",
-    "blender": (3, 4, 0),
-    "version": (1, 1, 6),
+    "blender": (4, 0, 0),
+    "version": (1, 2, 0),
     "category": "辣椒出品",
     "support": "COMMUNITY",
     "doc_url": "",
