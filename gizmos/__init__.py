e4c98d19731a3cfcc3059aa859b1e7dc45f23dc4
小萌新 <3209970865@qq.com>
2025-08-24 10:10:03 +0800

    Feat: modify code

--- gizmos/__init__.py
+++ gizmos/__init__.py
@@ -10,8 +10,8 @@ classes = (
     PreviewCameraAreaGizmo,
     PreviewCameraGizmos,
 
-    MotionCameraAdjustGizmo,
-    MotionCameraAdjustGizmos,
+    # MotionCameraAdjustGizmo,
+    # MotionCameraAdjustGizmos,
 )
 
 register_class, unregister_class = bpy.utils.register_classes_factory(classes)
